d17e0d5b68b351ceb8c3306542d8dfdfd5d41c85
damien rouxel <dam.rouxel@gmail.com>
2020-09-29 15:07:16 +0200

    TP1 part2

--- Xamarin-TP1/Xamarin-TP1/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
+++ Xamarin-TP1/Xamarin-TP1/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
@@ -17,8 +17,12 @@ namespace Xamarin_TP1 {
     public partial class MainPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Button btn_connect;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(MainPage));
+            btn_connect = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Button>(this, "btn_connect");
         }
     }
 }
