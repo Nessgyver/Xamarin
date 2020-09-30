df976642dd072a856727f17f64294a2a604c44b8
damien rouxel <dam.rouxel@gmail.com>
2020-09-30 10:42:15 +0200

    TP1 part3

--- Xamarin-TP1/Xamarin-TP1/MainPage.xaml.cs
+++ Xamarin-TP1/Xamarin-TP1/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,45 @@ namespace Xamarin_TP1
         public MainPage()
         {
             InitializeComponent();
+
+            this.btn_connect.Clicked += Connection_Clicked;
+        }
+
+        public void Connection_Clicked(object sender, EventArgs e)
+        {
+            string id = this.id.Text;
+            string password = this.password.Text;
+            bool rememberMe = this.remember_me.IsToggled;
+            bool isValidId = false;
+            bool isValidPassword = false;
+
+            if(id == null || id.Length<4)
+            {
+                this.error_id.IsVisible = true;
+                this.error_id.Text = "L'identifiant doit comporter 3 caractère minimum";
+            }else
+            {
+                this.error_id.IsVisible = false;
+                isValidId = true;
+            }
+            if(password == null || password.Length<6)
+            {
+                this.error_password.IsVisible = true;
+                this.error_password.Text = "Le mot de passe doit comporter 6 caractère minimum";
+            }
+            else
+            {
+                this.error_password.IsVisible = false;
+                isValidPassword = true;
+            }
+
+            if(isValidId && isValidPassword)
+            {
+                this.form.IsVisible = false;
+                this.tweet_feed.IsVisible = true;
+            }
+            Debug.WriteLine("btn-connect cliqué");
+
         }
     }
 }
